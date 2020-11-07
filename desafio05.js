a6a3a8a363f5af9681a61892e0b42cd51479468b
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-07 13:36:49 -0300

    Update desafio05.js
    desafio05 finalizado

--- desafio05.js
+++ desafio05.js
@@ -13,21 +13,60 @@ const transaction2 = {
     value: 10
 }
 
-function createTransaction (prop){
-    if(prop.type == 'credit'){
-        users.balance += prop.value
+function createTransaction (transaction){
+      users.transactions.push(transaction)
+    if(transaction.type === 'credit'){
+        users.balance += transaction.value
     }
-    else  (prop.type == 'debit')
-        users.balance -= prop.value
+    else  
+        users.balance -= transaction.value
     
-    return  users.transactions.push(prop.value)
 }
 
-createTransaction(transaction1)
-createTransaction(transaction2)
+function getHigherTransactionByType(typeOfTransiction){
+    let HigherTransaction = 0
+    let HigherValue = 0
+    for(props of users.transactions){
+        if(props.type == typeOfTransiction && props.value > HigherValue){
+            HigherValue = props.value
+            HigherTransaction = props
+        }
+    }
+    return HigherTransaction
+}
+
+function getAverageTransactionValue (){
+    let sum = 0
+    for (prop of users.transactions){
+        sum += prop.value
+}   
+    return sum/users.transactions.length
+}
+
+function getTransactionsCount (){
+    let count = {
+        credit : 0,
+        debit : 0
+    }
+    for (props of users.transactions){
+        if(props.type === 'credit'){
+            count.credit ++
+        }
+        else{
+            count.debit ++
+        }
+    }
+    return count
+}
+    
+
+createTransaction({ type: 'credit', value: 50 })
+createTransaction({ type: 'credit', value: 120 })
+createTransaction({ type: 'debit', value: 80 })
+createTransaction({ type: 'debit', value: 30 })
 
 console.log(users)
 
-
-
-    
+console.log(getHigherTransactionByType('credit'))
+console.log(getAverageTransactionValue())
+console.log(getTransactionsCount())
