9b0b2517aede9a4a2882114141ed4aef64951c48
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-07 10:56:36 -0300

    Update desafio04.js
    desafio04 completo

--- desafio04.js
+++ desafio04.js
@@ -43,3 +43,51 @@ for (let i = 0; i < usuarios.length; i++) {
         console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
     }
 }
+
+// Soma de despesas e receitas
+
+
+const UsuariosFinanceiro = [
+    {
+      nome: "Salvio",
+      receitas: [115.3, 48.7, 98.3, 14.5],
+      despesas: [85.3, 13.5, 19.9]
+    },
+    {
+      nome: "Marcio",
+      receitas: [24.6, 214.3, 45.3],
+      despesas: [185.3, 12.1, 120.0]
+    },
+    {
+      nome: "Lucia",
+      receitas: [9.8, 120.3, 340.2, 45.3],
+      despesas: [450.2, 29.9]
+    }
+  ];
+
+
+function calculaSaldo(receitas, despesas){
+    let saldoReceita = somaNumeros(receitas)
+    let saldoDespesa = somaNumeros(despesas)
+
+    return saldoReceita - saldoDespesa
+}
+
+function somaNumeros(numeros){
+    let soma = 0
+    for(let prop of numeros){
+        soma = soma + prop
+    }
+    return soma
+}
+
+for(let prop of UsuariosFinanceiro){
+    const saldo = calculaSaldo(prop.receitas,prop.despesas)
+    if(saldo > 0 ){
+        console.log(`O saldo de ${prop.nome} é de ${saldo.toFixed(2)}`)
+    }
+    else{
+        console.log(`O saldo de ${prop.nome} é de ${saldo.toFixed(2)}`)
+    }
+    
+}
