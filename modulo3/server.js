c29ec757a104fe82423b9e956b2dcc2d0ad29918
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-11 17:25:41 -0300

    Iniciando Modulo3
    Instalando dependencias

---
+++ modulo3/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const server = express()
+server.get("/", function(req,res){
+    return  res.send("Hiii")
+})
+server.listen(5000, function(){
+    console.log('server is running')
+})
