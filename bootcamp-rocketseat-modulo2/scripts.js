b764a1ad569339032b9faa578d74ebf0326a11ab
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-11 16:44:18 -0300

    Alteraçoes css
    desafio completo

--- bootcamp-rocketseat-modulo2/scripts.js
+++ bootcamp-rocketseat-modulo2/scripts.js
@@ -1,13 +1,13 @@
 const ModalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
-
 for(let card of cards){
-    card.addEventListener("click", function(){
+   console.log(card)
+     card.addEventListener("click", function(){
         const videoId = card.getAttribute("id")
         const iframe = ModalOverlay.querySelector("iframe")
         iframe.src = `https://www.youtube.com.br/embed/${videoId}`
         ModalOverlay.classList.add('active')
-    })
+    }) 
 }
 const closeModal = document.querySelector('.close-modal')
 closeModal.addEventListener("click", function(){
