a02fe658b01c7c2c8312eeea4a85f9f71fb0bbb7
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-10 21:29:11 -0300

    Rocketset
     Desafio 2-3: Página de posts e iframe

---
+++ bootcamp-rocketseat-modulo2/scripts.js
@@ -0,0 +1,19 @@
+const ModalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id")
+        const iframe = ModalOverlay.querySelector("iframe")
+        iframe.src = `https://www.youtube.com.br/embed/${videoId}`
+        ModalOverlay.classList.add('active')
+    })
+}
+const closeModal = document.querySelector('.close-modal')
+closeModal.addEventListener("click", function(){
+    ModalOverlay.classList.remove('active')
+    ModalOverlay.querySelector("iframe").src = ""
+})
+
+
+
