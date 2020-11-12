2acb6e9ac6a19bd48371786435a9d004ab4d735a
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-11-12 12:33:10 -0300

    Servidor
    Aprendendo servidores

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id : "U6RxX-cEHnM",
+        title: "Será que meu código é adequado? React.js ou Vue.js? Desenvolvimento Web. Next.js, Axios",
+        duration : "13 min",
+        Price: "Free"
+    },
+    {
+        id : "pbO3nnqyxlE",
+        title: "9 atitudes que nos fazem querer desistir (e como superar) | Code/Drops #63",
+        duration : "15 min",
+        Price: "Free"
+    },
+    {
+        id : "1SPkgcrIf74",
+        title: "Será que é possível aprender JavaScript com Desafios? | Code/Drops #62",
+        duration : "14 min",
+        Price: "Free"
+    },
+    {
+        id : "qrB94YucxFw",
+        title: "O que é Soft Skill e Hard Skill? Síndrome do Impostor, Carreira e Pressão | #PR 19",
+        duration : "32 min",
+        Price: "Free"
+    },
+    {
+        id : "DsbeclaivTo",
+        title: "9 atitudes que nos fazem querer desistir (e como superar) | Code/Drops #63",
+        duration : "15 min",
+        Price: "Free"
+    },
+    {
+        id : "HKE4ukzOz-U",
+        title: "Por que migramos para o Linux? Como testar apps iOS no Windows, GarageBand com RN?",
+        duration : "18 min",
+        Price: "Free"
+    },
+]
--- modulo3/server.js
+++ modulo3/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const server = express()
+const videos = require('./data.js')
 
 server.use(express.static('public'))
 
-server.set("view engine","html")
+server.set("view engine","njk")
 
 nunjucks.configure("views", {
     express: server
@@ -13,8 +14,11 @@ server.get("/", function(req,res){
     return  res.render("about")
 })
 server.get("/portfolio", function(req,res){
-    return  res.render("portfolio")
+    return  res.render("portfolio" , {items : videos})
 })
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
 server.listen(8080, function(){
     console.log('server is running')
 })
